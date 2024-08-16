50a807546eeb8091fe2b4a9be4fc542953447afe
qweasdf1231 <mraft5868@gmail.com>
2024-08-16 23:24:04 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+
+
+
+
+let res = prompt("Enter a city");
+
+switch (res) {
+  case "cairo":
+    document.write("5%");
+    break;
+  case "aswan":
+    document.write("30%");
+    break;
+  case "alex":
+    document.write("10%");
+    break;
+  default:
+    document.write("7%");
+}
